44950895bb8d46b6287d220fdda51cc7c50398b7
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-05-18 14:27:01 -0300

    Correação queryResult

--- src/app/controllers/DialogController.js
+++ src/app/controllers/DialogController.js
@@ -1,11 +1,11 @@
 class DialogController {
   async teste (req, res) {
-    const agent = req.body
-    const intent = agent.queryResult.intent.displayName
+    const agent = req.body.queryResult
+    const intent = agent.intent.displayName
 
-    // if (intent === 'busca.artigo') {
-    //   res.json({ fulfillmentText: `Resposta para ${agent.parameters.artigo}` })
-    // }
+    if (intent === 'busca.artigo') {
+      res.json({ fulfillmentText: `Resposta para ${agent.parameters.artigo}` })
+    }
 
     res.json({ fulfillmentText: `Seu intent é ${intent}` })
   }
