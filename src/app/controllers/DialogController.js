2293022a7cfc91b94d8c750986c4dea6b6016e6f
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-05-18 14:34:39 -0300

    Rest intent implementado

--- src/app/controllers/DialogController.js
+++ src/app/controllers/DialogController.js
@@ -4,7 +4,9 @@ class DialogController {
     const intent = agent.intent.displayName
 
     if (intent === 'busca.artigo') {
-      res.json({ fulfillmentText: `Resposta para ${agent.parameters.artigo}` })
+      if (agent.parameters.artigo === 'Rest') {
+        res.json({ fulfillmentText: 'Rest é um padrão de arquitetura baseado nos métodos Get, Post, Put e Delete' })
+      }
     }
 
     res.json({ fulfillmentText: `Seu intent é ${intent}` })
