f40c492a1a59556baaa5b2be5352e7cd4f97c1ae
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-05-18 13:26:36 -0300

    Primeiro webhook personalizado

--- src/app/controllers/DialogController.js
+++ src/app/controllers/DialogController.js
@@ -1,7 +1,11 @@
 class DialogController {
-  async teste (req, res) {
-    // const teste = req.body
-    res.json({ fulfillmentText: 'displayed&spoken response' })
+  async teste(req, res) {
+    const agent = req.body
+    const intent = agent.outputContexts[1].intent.displayName
+
+    if (intent === 'busca.artigo') {
+      res.json({ fulfillmentText: `Resposta para ${agent.parameters.artigo}` })
+    }
   }
 }
 
