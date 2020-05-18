12e23ce687358fb39a0469b9d8cdd3868eee545f
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-05-18 14:17:18 -0300

    Teste

--- src/app/controllers/DialogController.js
+++ src/app/controllers/DialogController.js
@@ -3,11 +3,11 @@ class DialogController {
     const agent = req.body
     const intent = agent.queryResult.intent.displayName
 
-    if (intent === 'busca.artigo') {
-      res.json({ fulfillmentText: `Resposta para ${agent.parameters.artigo}` })
-    }
+    // if (intent === 'busca.artigo') {
+    //   res.json({ fulfillmentText: `Resposta para ${agent.parameters.artigo}` })
+    // }
 
-    res.json({ fulfillmentText: 'Não funcionou' })
+    res.json({ fulfillmentText: `Seu intent é ${intent}` })
   }
 }
 
