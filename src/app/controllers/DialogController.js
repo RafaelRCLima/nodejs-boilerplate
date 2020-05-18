d42b9c6f527f8426aacdddb79848e31bcb9c1dfe
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-05-18 13:40:15 -0300

    Correção intent

--- src/app/controllers/DialogController.js
+++ src/app/controllers/DialogController.js
@@ -1,7 +1,7 @@
 class DialogController {
-  async teste(req, res) {
+  async teste (req, res) {
     const agent = req.body
-    const intent = agent.outputContexts[1].intent.displayName
+    const intent = agent.queryResult.outputContexts[1].intent.displayName
 
     if (intent === 'busca.artigo') {
       res.json({ fulfillmentText: `Resposta para ${agent.parameters.artigo}` })
